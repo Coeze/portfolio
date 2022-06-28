113a41cc9c680e9b4e835b8bd656697d0fc64f19
Daniel <coeze04@gmail.com>
2022-06-28 11:55:51 +0100

    adding another project

--- src/components/OtherProjects.js
+++ src/components/OtherProjects.js
@@ -7,7 +7,7 @@ function OtherProjects() {
         Other Projects - The code can be found on my github by clicking on the github icon below.  
         <div className={styles.projects}>
             <p>Qr code generator - used python tkinter and qrcode module</p>
-            <p>Notion API - used Node.js and Notion JavaScript API to organise to maximise productivity. </p>
+            <p>Notion API Project - used Node.js and Notion JavaScript API to organise to maximise productivity. </p>
         </div>
     </div>
   )
