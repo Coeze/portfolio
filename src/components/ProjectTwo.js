a4665f8feb3ad50b13890114ef6db0535d0827d7
coeze04 <coeze04@gmail.com>
2023-10-03 20:40:20 +0100

    change

--- src/components/ProjectTwo.js
+++ src/components/ProjectTwo.js
@@ -6,7 +6,7 @@ function ProjectTwo() {
   return (
     <div data-aos="flip-down"  data-aos-duration="2000" className={styles.heading}>
        <div className={styles.information}>
-      <span className={styles.span} >Project #2 &nbsp; - Database Design</span>
+      <span className={styles.span} >Project #4 &nbsp; - Database Design</span>
       <p>Developed a database for a research group who were monitoring the impact of fracking activity on earthquakes</p>
       <p>The database utilises spatial queries to quickly view earthquakes that occured within a certain distance from a site associated to a company</p>
       <p>In addition to this, I created a data warehouse, using a star schema, to support efficient data analysis</p>
